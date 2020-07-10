d95d209d5ad1b70ae7f3db064df91d9c5e1b728c
Christian Lewis <ctjl96@gmail.com>
2020-07-09 23:40:55 -0500

    Reorganized /utils

--- test/com/google/javascript/jscomp/runtime_tests/array_pattern_test.js
+++ test/com/google/javascript/jscomp/runtime_tests/array_pattern_test.js
@@ -20,8 +20,10 @@
  *
  * @author tbreisacher@google.com (Tyler Breisacher)
  */
+
 goog.require('goog.testing.jsunit');
 
+// ./closure-library/closure/bin/build/closurebuilder.py --root ./closure-library/ --root ./test/com/google/javascript/jscomp/runtime_tests/ -n goog.runtime_tests.array_pattern_test
 
 /**
  * Keeps track of how many times numbers() is called.
@@ -286,3 +288,5 @@ function testIterable_default() {
   [a='a', b, c] = gen();
   assertArrayEquals([0, undefined, 2], [a, b, c]);
 }
+
+goog.provide('goog.runtime_tests.array_pattern_test');
